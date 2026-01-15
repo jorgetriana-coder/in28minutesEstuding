e30cea95e8ae8f418d1eb57c62166d46019dce61
Jorge <jorge.triana@arvn.io>
2026-01-15 16:09:51 -0500

    Creacion de un endpiont en hellowordcontroller para trabajar los cambios de idioma mediante headers, agregando archivos de message.properties con el mismo fin

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWordController.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWordController.java
@@ -1,14 +1,27 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWordController {
+
+    private MessageSource messageSource;
+
+    public HelloWordController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "Hello World";
+
     }
 
     @GetMapping(path = "/hello-world-bean")
@@ -20,4 +33,12 @@ public class HelloWordController {
     public helloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new helloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default message", locale);
+    }
+
+
 }
