ccdbeb65c0a989ae96edb4a33db4e8ded0294ef2
Jorge <jorge.triana@arvn.io>
2026-01-15 19:52:45 -0500

    agregar Apis para versionanmineto y para filtros estaticos

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,12 +1,17 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -23,12 +28,16 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveAllUsers(@PathVariable int id) throws UserNotFoundException {
+    public EntityModel<User> retrieveUser(@PathVariable int id) throws UserNotFoundException {
         User user = service.findByID(id);
 
         if(user==null){
             throw new UserNotFoundException("id:" + id);
         }
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
         return null;
     }
     @DeleteMapping("/users/{id}")
